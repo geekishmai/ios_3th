088102bb4f82b493e92a9657b214719ca904c0cd
user <user@userdemac-mini.local>
2019-04-02 15:43:12 +0800

    未实现折叠收缩, 隐藏navbar

--- test1/ViewController.m
+++ test1/ViewController.m
@@ -17,8 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    _tableview= [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    // 在这里修改group/ plain样式
+    _tableview= [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
     _tableview.dataSource=self;
     _tableview.delegate=self;
     [self.view addSubview:_tableview];
@@ -33,6 +33,50 @@
 //    NSLog(@"%s",[_arrayData objectAtIndex:2]);
     [_tableview reloadData];
 }
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_arrayData count];
+}
+
+
+//初始化cell数据核心代码
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *str=@"cell";
+    //获得可复用的单元格
+    UITableViewCell* strCell=[_tableview dequeueReusableCellWithIdentifier:str];
+    if(strCell==nil){
+        strCell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:str];
+    }
+    NSString* contain=[NSString stringWithFormat:@"第%d行,第%d组",indexPath.row,indexPath.section];
+    strCell.textLabel.text=[_arrayData objectAtIndex:indexPath.row];  //可以换成其他数据源,以数组的形式 contain
+   
+    return strCell;
+
+}
+
+//设置每行cell高度
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 150;
+}
+//定义删除按钮
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return UITableViewCellEditingStyleDelete;
+}
+//滑动触发删除按钮动作
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    [_arrayData removeObjectAtIndex:indexPath.row];
+    [_tableview reloadData];
+}
+
+
+//选中单元格触发的动作
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
 
 -(void) creatBtn{
     _isEdit=NO;
@@ -55,58 +99,6 @@
 }
 
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return 1;
-}
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return [_arrayData count];
-}
 
-
-//初始化cell数据核心代码
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    NSString *str=@"cell";
-    //获得可复用的单元格
-    UITableViewCell* strCell=[_tableview dequeueReusableCellWithIdentifier:str];
-    if(strCell==nil){
-        strCell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:str];
-    }
-    NSString* contain=[NSString stringWithFormat:@"第%d行,第%d组",indexPath.row,indexPath.section];
-    strCell.textLabel.text=[_arrayData objectAtIndex:indexPath.row];  //可以换成其他数据源,以数组的形式 contain
-   
-    return strCell;
-//    return strCell;
-//        [tableView registerClass:[newTableViewCell class] forCellReuseIdentifier:@"cell"];
-//        newTableViewCell *cell = [_tableview dequeueReusableCellWithIdentifier:@"cell"];
-//    cell.label.text = @"1234";
-//    return cell;
-}
-//设置每行cell高度
-//-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-//    return 150;
-//}
-
-
-- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return UITableViewCellEditingStyleDelete;
-}
-//滑动触发动作
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
-    [_arrayData removeObjectAtIndex:indexPath.row];
-    [_tableview reloadData];
-}
-//选中单元格触发的动作
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    
-}
-
-
-
-//- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-//    return 10;
-//}
-//- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
-//    return @"title";
-//}
 
 @end
